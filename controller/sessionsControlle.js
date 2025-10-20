f7392ce949b62e2dec4605aed3e652a99175a23c
ALLAN KEVEN ALENCAR MESQUITA <allanmesquita6263@digitalcollege.internal>
2025-10-20 19:50:59 -0300

    Iniciando Jest

--- controller/sessionsControlle.js
+++ controller/sessionsControlle.js
@@ -8,16 +8,56 @@ class sessionsController{
 
         try {
             
-            
+            const newSessios = await prisma.sessions.create({
+                data:{
+                    movieId,
+                    theaterId,
+                    room,
+                    startTime,
+                    endTime,
+                    price
+                }
+            })
+            res.status(201).json({message:"Sessão criada com sucesso!" + newSessios});
+
+
         } catch (error) {
+            console.error(error)
             
+            res.status(500).json({message:"Erro ao criar a sessão"})
         }
 
     }
     async getAll(req,res){
+        const allsessions = await prisma.movies.findMany();
 
+        res.status(200).json(allsessions);
     }
-    async update(req,res){
+    async updateSessions(req,res){
+        const {id}= req.params;
+        const {movieId, theaterId, room, startTime, endTime, price} = req.body;
+
+        try {
+            const updateSessions =  await prisma.sessions.update({
+                where:{
+                    id: id
+                },
+                data:{
+                    movieId,
+                    theaterId,
+                    room,
+                    startTime,
+                    endTime,
+                    price
+                }
+            })
+
+            res.status(200).json(updateSessions);
+
+        } catch (error) {
+            res.status(500).json("Erro ao atualizar esta sessão!")
+            console.log(error);
+        }
 
     }
     async delete(req,res){
